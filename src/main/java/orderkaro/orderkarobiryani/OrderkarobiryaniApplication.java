f2d7d7ead631e03739bfba0050032a6b4a89cfea
nandinidasu22 <nandini.dasu22@gmail.com>
2023-11-03 07:54:32 +0530

    first commit

---
+++ src/main/java/orderkaro/orderkarobiryani/OrderkarobiryaniApplication.java
@@ -0,0 +1,13 @@
+package orderkaro.orderkarobiryani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderkarobiryaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderkarobiryaniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/orderkaro/orderkarobiryani/OrderkarobiryaniApplicationTests.java
@@ -0,0 +1,13 @@
+package orderkaro.orderkarobiryani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderkarobiryaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
